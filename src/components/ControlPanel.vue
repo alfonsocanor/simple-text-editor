05d73997d219079755e0176f2c3950f8f8a1c90b
alfonsocanor <canorluisa@gmail.com>
2020-03-05 08:08:35 -0300

    First commit

---
+++ src/components/ControlPanel.vue
@@ -0,0 +1,56 @@
+<template>
+   <div id="control-panel">
+        <div id="format-actions">
+            <button className="format-action" type="button"><b>B</b></button>
+            <button className="format-action" type="button"><i>I</i></button>
+            <button className="format-action" type="button"><u>U</u></button>
+            <button id="edit" className="format-action" type="button" v-on:click="getAction"><u>Edit</u></button>
+            <button id="save" className="format-action" type="button" v-on:click="getAction"><u>Save</u></button>
+            <button id="cancel" className="format-action" type="button" v-on:click="getAction"><u>Cancel</u></button>
+        </div>
+
+    </div>
+</template>
+
+<script> 
+export default {
+        name: "ControlPanel",
+        data() {
+            isEditing = false;
+            wasSaved = true;
+            wasCanceled = true;
+        },
+        methods: {
+            getAction: function(event){
+                if(event.target.id == 'edit'){
+                    this.isEditing = true
+                    this.$emit("editable", this.isEditing);
+                }
+                else if(event.target.id == 'save'){
+                    this.isEditing = false;
+                    this.$emit("saved", this.wasSaved);
+                    this.$emit("editable", this.isEditing);
+                }
+                else if(event.target.id == 'cancel'){
+                    this.isEditing = false;
+                    this.$emit("canceled", this.wasCanceled);
+                    this.$emit("editable", this.isEditing);
+                }
+            }
+        }
+    };
+</script>
+<style scoped>
+    #control-panel {
+        background-color: #fff;
+        height: 25px;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        padding-top: 5px;
+    }
+    #format-actions {
+        width: 300px;
+        margin-right: 400px;
+    }
+</style>
---
+++ src/components/FileZone.vue
@@ -0,0 +1,54 @@
+<template>
+   <div id="file-zone">
+        <div id="file">
+            <textarea @select="selectEvent" v-model="message"/>
+            <div v-bind:style=" action ? messageStyle : qwe">
+                {{ message }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FileZone",
+        data() {
+            return {
+                message: '',
+                action: true,
+                messageStyle: {
+                    border: '1px solid red',
+                    margin: '8px',
+                    padding: '8px'  
+                }
+            }
+        },
+        methods:{
+            selectEvent: function(e){
+                alert(
+                    e.target.value.substring(e.target.selectionStart, e.target.selectionEnd));
+            }
+        }
+        
+    };
+
+
+</script>
+
+<style scoped>
+    #file-zone {
+    background-color:#f1f0f0;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 5px;
+}
+
+    #file {
+        width: 600px;
+        flex-grow: 1;
+        background-color: #fff;
+        border: 1px solid #e4dede;
+    }
+</style>
